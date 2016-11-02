012f87d2f55ac292947515a1ce2cebb16e394505
Tianqi Zhang <tianzh@microsoft.com>
2016-11-02 16:46:06 +0800

    remove useless push-location from build.ps1

--- build.ps1
+++ build.ps1
@@ -6,13 +6,9 @@ $docs = Join-Path $scriptHome "docs"
 $tempFolder = Join-Path $scriptHome "aspnet"
 md -Force $tempFolder
 
-Push-Location $scriptHome
-Push-Location $tempFolder
-
 # run docfx metadata to generate YAML metadata
 & docfx metadata $docs\docfx.json
 if($LASTEXITCODE -ne 0)
 {
-    Pop-Location
     exit 1
 }
